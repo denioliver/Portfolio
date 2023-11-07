5167d9c36deeee80ddd591a2ae3a11959c707e82
Denivan Oliveira <denyoliver777@gmail.com>
2023-11-06 22:20:59 -0300

    feat - organizando rotas

--- src/content/Content.jsx
+++ src/content/Content.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, HashRouter } from "react-router-dom"
 import Home from "../compenents/Home"
 import Projetos from "../compenents/Projetos"
 import Contatos from "../compenents/Contatos"
@@ -7,12 +7,14 @@ import Sobre from "../compenents/Sobre"
 function Content(){
     return (
         <div>
-            <Routes>
-                <Route path="/portfolio" element={ <Home /> } />
-                <Route path="/portfolio/Sobre"  element={ <Sobre />} />
-                <Route path="/portfolio/Projetos"  element={ <Projetos />} />
-                <Route path="/portfolio/Contatos"  element={ <Contatos />} />
-            </Routes>
+            <HashRouter>
+                <Routes>
+                    <Route path="/portfolio" element={ <Home /> } />
+                    <Route path="/portfolio/Sobre"  element={ <Sobre />} />
+                    <Route path="/portfolio/Projetos"  element={ <Projetos />} />
+                    <Route path="/portfolio/Contatos"  element={ <Contatos />} />
+                </Routes>
+            </HashRouter>
         </div>
     )
 }
