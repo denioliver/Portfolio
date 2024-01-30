2ffa28e1db52d4c38e0e492fea7f3bffdcf146be
Denivan Oliveira <denyoliver777@gmail.com>
2024-01-29 22:01:54 -0300

    feat: css - media query

--- src/compenents/Contatos.jsx
+++ src/compenents/Contatos.jsx
@@ -11,25 +11,25 @@ function Contatos(){
             <div className={style.containerUl}>
                 <ul>
                     <li>
-                        <a href="">
+                        <a href="https://wa.me/5575991489231" target="blank">
                             <img src={logoZap} alt="" />
                             <p>(75)9.91489231</p>
                         </a>
                     </li>
                     <li>
-                        <a href="">
+                        <a href="mailto:denyoliver777@gmail.com">
                             <img src={logoEmail} alt="" />
                             <p>denyoliver777@gmail.com</p>
                         </a>
                     </li>
                     <li>
-                        <a href="">
+                        <a href="https://www.linkedin.com/in/denivan-oliveira-237a66249" target='blank'>
                             <img src={logoLin} alt="" />
-                            <p>www.linkedin.com/in/denivan-oliveira-237a66249</p>
+                            <p>https://www.linkedin.com/in/denivan-oliveira-237a66249</p>
                         </a>
                     </li>
                     <li>
-                        <a href="">
+                        <a href="https://github.com/denioliver" target="blank" >
                             <img src={logoGithub} alt="" />
                             <p>https://github.com/denioliver</p>
                         </a>
