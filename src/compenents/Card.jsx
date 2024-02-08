d94fd29bb7ab735e437ac32df8c4ee24d02b7382
Denivan Oliveira <denyoliver777@gmail.com>
2024-02-08 01:35:15 -0300

    feat: card dos projetos

---
+++ src/compenents/Card.jsx
@@ -0,0 +1,31 @@
+import Projetos from '../list/projetos.js'
+import style from '../styles/Card.module.css'
+
+function Card() {
+
+  return (
+    <ul className={ style.ulContainer } >
+      {Projetos.map((projeto) => (
+        <li
+          className={ style.liCard }
+          key={ projeto.name }
+        >
+          <img
+            style={ { width:'350px' } }
+            src={ projeto.image }
+            alt="primeiro Portifolio"
+            />
+          <h3>{ projeto.name }</h3>
+          <a
+            href={ projeto.url }
+            target="blank"
+          >
+            Acessar
+          </a>
+        </li>
+      ))}
+    </ul>
+  )
+};
+
+export default Card
---
+++ src/list/projetos.js
@@ -0,0 +1,29 @@
+import imgPortfolioAntigo from '../img/portfolio-antigo.png';
+import imgProjetoFormulario from '../img/projeto-formulario.png';
+import imgProjetoRedeSociais from '../img/projeto-redes-sociais.png';
+import imgProjetoCalculadora from '../img/projeto-calculadora.png';
+
+const Projetos = [
+    {
+        name:'Projeto: Calculadora',
+        image: imgProjetoCalculadora,
+        url:'https://denioliver.github.io/calculadora/',
+    },
+    {
+        name:'Primeiro Portfolio',
+        image: imgPortfolioAntigo,
+        url:'https://denioliver.github.io/antigo-portfolio/',
+    },
+    {
+        name:'Projeto: Formulário',
+        image: imgProjetoFormulario,
+        url:'https://denioliver.github.io/projeto-formulario/',
+    },
+    {
+        name:'Projeto: Redes Sociais',
+        image: imgProjetoRedeSociais,
+        url:'https://denioliver.github.io/projeto-social/',
+    },
+]
+
+export default Projetos;
